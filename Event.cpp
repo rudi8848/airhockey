97216d6b96e7fcb9c26e4e087cd18579e7b09ea7
rudi8848 <anna.v8848@gmail.com>
2019-05-20 11:52:33 +0000

    added mouse clicks

--- Event.cpp
+++ Event.cpp
@@ -19,7 +19,43 @@ void Event::onEvent(SDL_Event *e)
             onExit();
             break;
         }
-
+        case SDL_KEYDOWN:
+        {
+            onKeyDown(e->key.keysym.sym, e->key.keysym.mod, e->key.keysym.scancode);
+            break;
+        }
+        case SDL_KEYUP:
+        {
+            onKeyUp(e->key.keysym.sym, e->key.keysym.mod, e->key.keysym.scancode);
+            break;
+        }
+        case SDL_MOUSEBUTTONDOWN:
+        {
+            switch (e->button.button)
+            {
+                case SDL_BUTTON_LEFT:
+                {
+                    onLButtonDown(e->button.x, e->button.y);
+                    break;
+                }
+                case SDL_BUTTON_RIGHT:
+                {
+                    onRButtonDown(e->button.x, e->button.y);
+                    break;
+                }
+                case SDL_BUTTON_MIDDLE:
+                {
+                    onMButtonDown(e->button.x, e->button.y);
+                    break;
+                }
+            }
+            break;
+        }
+        case SDL_MOUSEMOTION:
+        {
+            onMouseMove(e->motion.x, e->motion.y, e->motion.xrel, e->motion.yrel, (e->motion.state&SDL_BUTTON(SDL_BUTTON_LEFT)) != 0,(e->motion.state&SDL_BUTTON(SDL_BUTTON_RIGHT)) != 0,(e->motion.state&SDL_BUTTON(SDL_BUTTON_MIDDLE)) != 0 );
+            break;
+        }
 
         default:
             break;
@@ -27,6 +63,43 @@ void Event::onEvent(SDL_Event *e)
 }
 
 void Event::onExit()
+{
+
+}
+
+void Event::onKeyDown(SDL_Keycode sym, Uint16 mod, Uint16 scancode)
+{
+    switch(sym)
+    {
+        case SDLK_ESCAPE:
+        {
+            onExit();
+            break;
+        }
+    }
+}
+
+void Event::onKeyUp(SDL_Keycode sym, Uint16 mod, Uint16 scancode)
+{
+
+}
+
+void Event::onLButtonDown(int xm, int ym)
+{
+
+}
+
+void Event::onRButtonDown(int xm, int ym)
+{
+
+}
+
+void Event::onMButtonDown(int xm, int ym)
+{
+
+}
+
+void Event::onMouseMove(int xm, int ym, int relx, int rely, bool left, bool right, bool middle)
 {
 
 }
--- Event.hpp
+++ Event.hpp
@@ -11,6 +11,12 @@ class Event
 
     virtual void onEvent(SDL_Event *e);
     virtual void onExit();
+    virtual void onKeyDown(SDL_Keycode sym, Uint16 mod, Uint16 scancode);
+    virtual void onKeyUp(SDL_Keycode sym, Uint16 mod, Uint16 scancode);
+    virtual void onLButtonDown(int xm, int ym);
+    virtual void onRButtonDown(int xm, int ym);
+    virtual void onMButtonDown(int xm, int ym);
+    virtual void onMouseMove(int xm, int ym, int relx, int rely, bool left, bool right, bool middle);
 
     private:
 
